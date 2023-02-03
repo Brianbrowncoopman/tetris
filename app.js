eec3d547608ec26caf3a67b5effcf764863354c3
Brian Brown Coopman <brianbrowncoopman@gmail.com>
2023-02-02 23:03:18 -0300

    tetris poco style

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let squares = Array.from(document.querySelectorAll(".grid div"));
-  const ScoreDisplay = document.querySelector("#score");
-  const StartBtn = document.querySelector("#start-button");
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-button");
   const width = 10;
+  let nextRandom = 0;
+  let timerId;
+  let score = 0;
+  const colors = ["orange", "red", "purple", "green", "blue"];
 
   //the tetrominoes
   const lTetromino = [
@@ -60,6 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function draw() {
     current.forEach((index) => {
       squares[currentPosition + index].classList.add("tetromino");
+      squares[currentPosition + index].style.backgroundColor = colors[random];
     });
   }
 
@@ -67,18 +72,19 @@ document.addEventListener("DOMContentLoaded", () => {
   function unDraw() {
     current.forEach((index) => {
       squares[currentPosition + index].classList.remove("tetromino");
+      squares[currentPosition + index].style.backgroundColor = "";
     });
   }
 
   //make move down every second
-  timerId = setInterval(moveDown, 1000);
+  //timerId = setInterval(moveDown, 1000);
 
   //assing function to keyboard
   function control(e) {
     if (e.keyCode === 37) {
       moveLeft();
     } else if (e.keyCode === 38) {
-      //rotate()
+      rotate();
     } else if (e.keyCode === 39) {
       moveRight();
     } else if (e.keyCode === 40) {
@@ -105,11 +111,15 @@ document.addEventListener("DOMContentLoaded", () => {
       current.forEach((index) =>
         squares[currentPosition + index].classList.add("taken")
       );
-      //start a tetromino
-      random = Math.floor(Math.random() * theTetrominoes.length);
+      //start a tetromino falling
+      random = nextRandom;
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
       current = theTetrominoes[random][currentRotation];
       currentPosition = 4;
       draw();
+      displayShape();
+      addScore();
+      gameOver();
     }
     draw();
   }
@@ -150,5 +160,100 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition -= 1;
     }
     draw();
+  }
+
+  //rotate tetromino
+  function rotate() {
+    unDraw();
+    currentRotation++;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
+    draw();
+  }
+
+  //show next tetr... in mini grid
+  const displaySquares = document.querySelectorAll(".mini-grid div");
+  const displayWidth = 4;
+  let displayIndex = 0;
+
+  //tetro... without rotation
+  const upNextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2], //l tetr
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], //z trtr
+    [1, displayWidth, displayWidth + 1, displayWidth + 2], //t tetro
+    [0, 1, displayWidth, displayWidth + 1], //o tetro
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1], //i tetro
+  ];
+
+  //display all shapes in the minigrid
+  function displayShape() {
+    //remove trace of tro
+    displaySquares.forEach((squares) => {
+      squares.classList.remove("tetromino");
+      squares.style.backgroundColor = "";
+    });
+    upNextTetrominoes[nextRandom].forEach((index) => {
+      displaySquares[displayIndex + index].classList.add("tetromino");
+      displaySquares[displayIndex + index].style.backgroundColor =
+        colors[nextRandom];
+    });
+  }
+
+  //add functionality to the button
+  startBtn.addEventListener("click", () => {
+    if (timerId) {
+      clearInterval(timerId);
+      timerId = null;
+    } else {
+      draw();
+      timerId = setInterval(moveDown, 1000);
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+      displayShape();
+    }
+  });
+
+  //add scrore
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [
+        i,
+        i + 1,
+        i + 2,
+        i + 3,
+        i + 4,
+        i + 5,
+        i + 6,
+        i + 7,
+        i + 8,
+        i + 9,
+      ];
+
+      if (row.every((index) => squares[index].classList.contains("taken"))) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+        row.forEach((index) => {
+          squares[index].classList.remove("taken");
+          squares[index].classList.remove("tetromino");
+          squares[index].style.backgroundColor = "";
+        });
+        const squaresRemoved = squares.splice(i, width);
+        squares = squaresRemoved.concat(squares);
+        squares.forEach((cell) => grid.appendChild(cell));
+      }
+    }
+  }
+
+  //game over
+  function gameOver() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      scoreDisplay.innerHTML = "end";
+      clearInterval(timerId);
+    }
   }
 });
